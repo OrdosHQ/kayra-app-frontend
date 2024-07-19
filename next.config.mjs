05d4bdd04f4d320fc559480f835ff3153895520d
thrmdyy <thrmdy@gmail.com>
2024-07-20 00:14:12 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -21,10 +21,25 @@ const nextConfig = {
             {
                 source: '/:path*',
                 headers: [
-                    { key: 'Cross-Origin-Opener-Policy', value: 'unsafe-none' },
                     {
                         key: 'Cross-Origin-Embedder-Policy',
-                        value: 'unsafe-none',
+                        value: 'require-corp',
+                    },
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, PUT, DELETE, OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: '*',
                     },
                 ],
             },
