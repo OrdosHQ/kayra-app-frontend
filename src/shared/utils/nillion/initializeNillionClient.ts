ebf38b4d3b7938c22cd7e54c058e9978c8dfe12d
thrmdyy <thrmdy@gmail.com>
2024-07-23 19:34:37 +0400

    Update initializeNillionClient.ts

--- src/shared/utils/nillion/initializeNillionClient.ts
+++ src/shared/utils/nillion/initializeNillionClient.ts
@@ -1,9 +1,12 @@
 import { config } from '@/shared/constants/nillion';
 import * as nillion from '@nillion/client-web';
+import { generateSalt } from '../generate-salt';
 
 export const initializeNillionClient = (
     userKey: nillion.UserKey,
 ): nillion.NillionClient => {
-    const nodeKey = nillion.NodeKey.from_seed('');
+    const seed = generateSalt();
+    const nodeKey = nillion.NodeKey.from_seed(seed);
+    console.log(nodeKey, 'nodeKey');
     return new nillion.NillionClient(userKey, nodeKey, config.bootnodes);
 };
